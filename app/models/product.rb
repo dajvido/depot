1aa0f684b78782b322ee7c865ba85cb669e4196d
dajvido <dawid.jaskot@gmail.com>
2015-03-30 12:33:08 +0200

    Change error validation message for Products title

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, allow_blank: true, uniqueness: true, length: { minimum: 10 }
+  validates :title, allow_blank: true, uniqueness: true
+  validates_length_of :title, allow_blank: true, minimum: 10, too_short: "must be at least 10 characters long"
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
